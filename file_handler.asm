ec416c1f020437d968d74d8c9e3c96c3954f480f
Mohamed25885 <memo_868@yahoo.com>
2022-12-21 22:33:19 +0200

    open and close la

--- file_handler.asm
+++ file_handler.asm
@@ -82,3 +82,27 @@ _writeFromToPosFile:
     syscall
     pop r11
     ret
+
+
+_open_and_read_file:
+    ; open the file
+    mov eax, 5 ;sys_open(file_name, flags, mode)
+    mov ebx, dword [ebp + 16]
+    mov ecx, 0             ;for read only access
+    mov edx, 0777          ;read, write and execute by all
+    int 80h
+    ;add esp, 8
+
+    mov  [fd_in], eax
+
+    mov eax, 3
+    mov ebx, [fd_in]
+    mov ecx, info
+    mov edx, 0xfff
+    int 0x80
+    ret
+_closeFile:
+    mov eax, 6
+    mov ebx, [fd_in]
+    int  0x80 
+    ret
